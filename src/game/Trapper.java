ff45ecf53260f064327d6f6f2abb8973fa41eace
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2018-01-06 21:09:00 +0600

    Update Trapper.java

--- src/game/Trapper.java
+++ src/game/Trapper.java
@@ -1,4 +1,32 @@
 package game;
 
-public class Trapper {
+import util.NetworkUtil;
+
+public class Trapper implements Player {
+    Grid grid;
+    NetworkUtil server;
+    boolean isHuman;
+
+    public Trapper(Grid grid, NetworkUtil server) {
+        this.grid = grid;
+        this.server = server;
+        isHuman = false;
+    }
+
+    public Trapper(Grid grid) {
+        this.grid = grid;
+        isHuman = true;
+    }
+
+    public Cell makeMove() {
+        Cell cell;
+        if (isHuman) {
+            cell = (Cell) server.read();
+        } else {
+            cell = moveByAI();
+        }
+    }
+    public Cell moveByAI() {
+
+    }
 }
