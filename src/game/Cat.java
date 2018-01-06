8a58bda92f1ee12c46c1f73b1a72236f5957c5a8
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2018-01-06 21:08:44 +0600

    Update Cat.java

--- src/game/Cat.java
+++ src/game/Cat.java
@@ -1,4 +1,32 @@
 package game;
 
-public class Cat {
+import util.NetworkUtil;
+
+public class Cat implements Player {
+    Grid grid;
+    NetworkUtil server;
+    boolean isHuman;
+
+    public Cat(Grid grid, NetworkUtil server) {
+        this.grid = grid;
+        this.server = server;
+        isHuman = false;
+    }
+
+    public Cat(Grid grid) {
+        this.grid = grid;
+        isHuman = true;
+    }
+
+    public Cell makeMove() {
+        Cell cell;
+        if (isHuman) {
+            cell = (Cell) server.read();
+        } else {
+            cell = moveByAI();
+        }
+    }
+    public Cell moveByAI() {
+
+    }
 }
