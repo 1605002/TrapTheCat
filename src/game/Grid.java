dad84b0a27790dd315ab9749198830f1bdd31315
1605002 <31519659+1605002@users.noreply.github.com>
2018-01-06 21:12:42 +0600

    Update Grid.java
    Update in grid

--- src/game/Grid.java
+++ src/game/Grid.java
@@ -1,4 +1,55 @@
 package game;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+import java.util.Scanner;
+
 public class Grid {
+    static final Paint BLOCKED = Color.DARKGREEN;
+    static final Paint OPEN = Color.LIGHTGREEN;
+    static final Paint BILAIASE = Color.RED;
+
+    private Scene scene;
+    private Britto[][] brittos;
+    private Cell catPosition;
+    private Group root;
+    private Turn turn;
+
+    public Grid(Scene scene, Group root) {
+
+        this.scene = scene;
+        this.root = root;
+
+        brittos = new Britto[11][11];
+
+        for(int i = 0; i < 11; i++) {
+            for(int j = 0; j < 11; j++) {
+                Circle circle = new Circle();
+                brittos[i][j] = new Britto(this.scene, circle, i, j, false);
+                this.root.getChildren().add(brittos[i][j].getCircle());
+            }
+        }
+
+        catPosition = new Cell(3, 6);
+
+    }
+
+    public void block(int x, int y) {
+        brittos[x][y].setStatus(true);
+        brittos[x][y].getCircle().setFill(BLOCKED);
+    }
+
+    public void moveTo(int x, int y) {
+
+        int oldX = catPosition.getX();
+        int oldY = catPosition.getY();
+        brittos[oldX][oldY].getCircle().setFill(OPEN);
+        catPosition = new Cell(x, y);
+        brittos[x][y].getCircle().setFill(BILAIASE);
+    }
+
 }
