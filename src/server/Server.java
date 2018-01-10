38448599b92a0ba26f19468de3cf3a13921becdf
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2018-01-10 17:44:59 +0600

    Update Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,40 @@
 package server;
 
+import game.Cell;
+import util.NetworkUtil;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Server {
+
+    public static void main(String[] args) {
+        Queue<NetworkUtil> trappers = new LinkedList<>();
+        Queue<NetworkUtil> cats = new LinkedList<>();
+
+        try {
+            ServerSocket ss = new ServerSocket(44444);
+
+            while (true) {
+                Socket cs = ss.accept();
+                NetworkUtil nc = new NetworkUtil(cs);
+
+                Cell information = (Cell) nc.read();
+
+                if (information.getX()==0) trappers.add(nc);
+                else cats.add(nc);
+
+                if (!trappers.isEmpty() && !cats.isEmpty()) {
+                    new Game(trappers.peek(), cats.peek());
+                    trappers.remove();
+                    cats.remove();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Server status: " + e);
+        }
+    }
 }
