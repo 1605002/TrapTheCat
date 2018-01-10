253b66615c4a7fd5018c870e2348737a5182aa3f
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2018-01-10 15:41:34 +0600

    Update Player.java

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,6 +1,23 @@
 package game;
 
-public interface Player {
-    Cell makeMove();
-    Cell moveByAI();
+import util.NetworkUtil;
+
+public abstract class Player {
+    private Grid grid;
+    private NetworkUtil server;
+
+    public Player(Grid grid, NetworkUtil server) {
+        this.grid = grid;
+        this.server = server;
+    }
+
+    public Player(Grid grid) {
+        this.grid = grid;
+    }
+
+    abstract public Cell makeMove();
+
+    public Cell readFromServer() {
+        return (Cell) server.read();
+    }
 }
