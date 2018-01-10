f097fdcac618eb5d8e460bac8a3442e77b99a979
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2018-01-10 20:11:16 +0600

    Update Britto.java

--- src/game/Britto.java
+++ src/game/Britto.java
@@ -9,53 +9,113 @@ import javafx.scene.shape.Circle;
 
 public class Britto {
 
+    private static Cell clickedCell;
+    private static Object lock = new Object();
+
+    public static void setClickedCell(Cell cell) {
+        synchronized (lock) {
+            clickedCell = cell;
+        }
+    }
+
+    public static Cell getClickedCell() {
+        synchronized (lock) {
+            return clickedCell;
+        }
+    }
+
+    static int dxEven[] = {0, -1, -1, 0, 1, 1};
+    static int dyEven[] = {1, 0, -1, -1, -1, 0};
+
+    static int dxOdd[] = {0, -1, -1, 0, 1, 1};
+    static int dyOdd[] = {1, 1, 0, -1, 0, 1};
+
+    private int player;
+    public int getPlayer() { return player; }
+
     private Scene scene;
 
+    private Circle circle;
     public Circle getCircle() {
         return circle;
     }
 
-    private Circle circle;
     private int row;
+    private int column;
+    public int getRow() { return row; }
+    public int getColumn() { return column; }
 
+    private boolean status = false;
     public void setStatus(boolean status) {
         this.status = status;
     }
-
-    private int column;
-
     public boolean isStatus() {
         return status;
     }
 
-    private boolean status = false;
     private Turn turn;
+    private Cell catPosition;
 
+    public boolean isValidCell() {
+        if (turn.getTurn()!=getPlayer()) return false;
+        if (isStatus()==true) return false;
 
-    public Britto() {
+        if (getPlayer()==0) {
+            return !status;
+        }
+        else {
+            if (catPosition.getX()%2==0) {
+                for (int i = 0; i < 6; i++) {
+                    if (catPosition.getX()+dxEven[i]==row &&
+                            catPosition.getY()+dyEven[i]==column) return true;
+                }
+            } else {
+                for (int i = 0; i < 6; i++) {
+                    if (catPosition.getX()+dxOdd[i]==row &&
+                            catPosition.getY()+dyOdd[i]==column) return true;
+                }
+            }
 
+            return false;
+        }
     }
 
-    public Britto(Scene scene, Circle circle, int row, int column, boolean status, Turn turn) {
+
+    public Britto(Scene scene,
+                  Circle circle,
+                  int row,
+                  int column,
+                  boolean status,
+                  Turn turn,
+                  int player,
+                  Cell catPosition) {
+
+        this.catPosition = catPosition;
+        this.player = player;
+        this.scene = scene;
+        this.circle = circle;
+        this.row = row;
+        this.column = column;
+        this.status = status;
+        this.turn = turn;
 
         circle.setCenterX(50+40*row+20*(column%2));
         circle.setCenterY(50+40*column);
         circle.setRadius(19);
-        circle.setFill(Color.rgb(202, 240, 70));
-        this.turn = turn;
+        circle.setFill(Grid.OPEN);
 
         circle.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                setStatus(true);
-                circle.setFill(Color.DARKGREEN);
+                if (isValidCell())
+                    setClickedCell(new Cell(getRow(), getColumn()));
             }
         });
 
         circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                if(isStatus() == false)
+                if(isValidCell())
                     scene.setCursor(Cursor.HAND);
             }
         });
@@ -66,11 +126,6 @@ public class Britto {
                 scene.setCursor(Cursor.DEFAULT);
             }
         });
-
-        this.scene = scene;
-        this.circle = circle;
-        this.row = row;
-        this.column = column;
-        this.status = status;
     }
+
 }
