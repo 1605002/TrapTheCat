7d68774f5af8b2a5ac151b680875401fb15dfb7e
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2018-01-15 14:56:11 +0600

    Update Cell.java

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -14,4 +14,12 @@ public class Cell implements Serializable {
         return x;
     }
     public int getY() { return y; }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
